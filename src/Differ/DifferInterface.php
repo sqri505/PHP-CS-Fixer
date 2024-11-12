d47cf2b9c9ae07e64836ed5569673b14b096cc70
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2024-11-12 14:33:37 +0100

    bulk: Differ

--- src/Differ/DifferInterface.php
+++ src/Differ/DifferInterface.php
@@ -16,6 +16,8 @@ namespace PhpCsFixer\Differ;
 
 /**
  * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ * 
+ * @readonly
  */
 interface DifferInterface
 {
--- src/Differ/FullDiffer.php
+++ src/Differ/FullDiffer.php
@@ -20,6 +20,7 @@ use SebastianBergmann\Diff\Output\StrictUnifiedDiffOutputBuilder;
 /**
  * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
  *
+ * @readonly
  * @internal
  */
 final class FullDiffer implements DifferInterface
